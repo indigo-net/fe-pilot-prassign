c89eab5d63525f920d1fa60e704b92b2a62d03cd
yunshin <2younsin@gmail.com>
2024-10-12 23:38:49 +0900

    add: prettier 추가 및 설정 & lint 추가 설정

--- README.md
+++ README.md
@@ -5,7 +5,6 @@ _이 프로젝트는 [server-sync]() 프로젝트의 일부인 'Prassign' 기능
 <br/>
 
 # Prassign (Frontend) 개발 문서
+
 - 해당 저장소에서는 Prassign 기능 구현에 필요한 **프론트엔드** 코드를 관리합니다.
-- _(* 구체적인 프로젝트 내용은 [prassign 설계 문서](https://github.com/indigo-net/indigo-net/blob/main/projects/server-sync/pilot-prassign.md) 를 참고해주세요.)_
-
-
+- _(\* 구체적인 프로젝트 내용은 [prassign 설계 문서](https://github.com/indigo-net/indigo-net/blob/main/projects/server-sync/pilot-prassign.md) 를 참고해주세요.)_
--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,10 @@
+// eslint.config.js
+
 import js from '@eslint/js'
-import globals from 'globals'
+import prettierPlugin from 'eslint-plugin-prettier'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
@@ -12,10 +15,19 @@ export default tseslint.config(
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parser: tseslint.parser,
+      parserOptions: {
+        project: [
+          './tsconfig.json',
+          './tsconfig.node.json',
+          './tsconfig.app.json',
+        ],
+      },
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      prettier: prettierPlugin,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -23,6 +35,18 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      'prettier/prettier': 'error', // Prettier 오류를 ESLint 오류로 표시
+
+      // 추가된 규칙들
+      'no-console': 'warn',
+      camelcase: 'error',
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/consistent-type-imports': 'error',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/prefer-as-const': 'error',
     },
   },
 )
