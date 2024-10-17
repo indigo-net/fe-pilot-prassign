41c888c328152dd6eda7bd3f898d53fdc498a80b
yunshin <2younsin@gmail.com>
2024-10-17 19:40:03 +0900

    delete: 불필요한 코드 제거

--- src/components/manager/user-list.tsx
+++ src/components/manager/user-list.tsx
@@ -1,36 +1,6 @@
 import type { PropsWithChildren } from 'react'
 import { S } from './user-list.s'
 
-// const UserList = ({ users }: { users: UserType[] }) => {
-//   const mapCodeToColor: Record<
-//     StatusCodeType,
-//     'violet' | 'skyBlue' | 'purple' | 'pink' | 'alert'
-//   > = {
-//     0: 'violet',
-//     1: 'skyBlue',
-//     2: 'purple',
-//   }
-
-//   return (
-//     <S.ListWrapper>
-//       <S.ListBase>
-//         {users.map((user) => {
-//           const statusLabel = mapCodeToLabel(user.status) || ''
-//           const statusColor = mapCodeToColor[user.status] || 'skyBlue'
-//           return (
-//             <S.UserItem key={user.uuid}>
-//               <Typography variant="caption">{user.userName}</Typography>
-//               <Typography variant="captionBold">
-//                 <HighlightText color={statusColor}>{statusLabel}</HighlightText>
-//               </Typography>
-//               <Checkbox disabled={user.status !== 1} />
-//             </S.UserItem>
-//           )
-//         })}
-//       </S.ListBase>
-//     </S.ListWrapper>
-//   )
-// }
 const ListArea = ({ children }: PropsWithChildren) => {
   return (
     <S.ListWrapper>
--- src/components/qr-box.tsx
+++
@@ -1,5 +0,0 @@
-import { S } from './qr-box.s'
-const QRBox = () => {
-  return <S.Wrapper />
-}
-export default QRBox
