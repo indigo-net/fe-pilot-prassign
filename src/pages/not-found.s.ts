eb7dcf241aac21e814aa5842e00155a29a542e2c
yunshin <2younsin@gmail.com>
2024-10-27 23:18:38 +0900

    design: NotFound 페이지

---
+++ src/pages/not-found.s.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+const PageContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 50px;
+`
+
+export const S = {
+  PageContainer,
+}
--- src/pages/not-found.tsx
+++ src/pages/not-found.tsx
@@ -1,4 +1,18 @@
+import { useNavigate } from 'react-router-dom'
+import Button from '../components/common/button'
+import Typography from '../components/common/typography'
+import { S } from './not-found.s'
+
 const NotFound = () => {
-  return <h1>Not Found</h1>
+  const navigate = useNavigate()
+  const onClickHome = () => {
+    navigate('/')
+  }
+  return (
+    <S.PageContainer>
+      <Typography>404: Not Found</Typography>
+      <Button onClick={onClickHome}>홈으로</Button>
+    </S.PageContainer>
+  )
 }
 export default NotFound
